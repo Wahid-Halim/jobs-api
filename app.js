beb28f27259560471348cb78ac0e02d8a96db267
Wahid Halim <wahidkeo@gmail.com>
2025-11-13 11:04:14 +0430

    connect to DB

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+
+// connect db
+const connectDB = require("./db/connect");
+
 // routes
 const authRoutes = require("./routes/authRoutes");
 const jobsRoutes = require("./routes/jobsRoutes");
@@ -24,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
