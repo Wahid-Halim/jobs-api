e263332854778726d5b2bb5020a9ea30e0368bc5
Wahid Halim <wahidkeo@gmail.com>
2025-11-20 10:07:57 +0430

    generate jwt token and send in res

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
+
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
