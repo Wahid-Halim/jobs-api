f14d8c856fc4a99cdbc7bb162e371a5b69189bf8
Wahid Halim <wahidkeo@gmail.com>
2025-11-26 16:36:26 +0430

    add duplicate custom error in error-handler middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,29 @@
 const { CustomAPIError } = require("../errors/index");
 const { StatusCodes } = require("http-status-codes");
+
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, try again later",
+  };
+
+  // Custom API Error
   if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+    return res.status(customError.statusCode).json({ msg: customError.msg });
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  // Duplicate key error (MongoDB / Mongoose)
+  if (err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+
+    return res.status(customError.statusCode).json({ msg: customError.msg });
+  }
+
+  // Default fallback
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
